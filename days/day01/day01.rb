3bc13a88d576c7d325c51b2dd8ef77e5ed8c1a25
Sebastien Faure <sebastien@squadracer.com>
2023-12-04 11:10:24 +0100

    Add Day01 and Day02

---
+++ days/day01/day01.rb
@@ -0,0 +1,21 @@
+module Day01
+  def self.parse(input)
+    input = input.split("\n")
+  end
+
+  def self.part1(input)
+    parse(input).sum {|line| digits = line.scan(/\d/); [digits.first, digits.last].join.to_i}
+  end
+
+  def self.part2(input)
+    str_to_digits = {"one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6,
+      "seven" => 7, "eight" => 8, "nine" => 9}
+    parse(input).sum do |line|
+      first = line[/(#{["\\d", *str_to_digits.keys].join("|")})/]
+      first = str_to_digits[first] unless first[/\d/]
+      last = line.reverse[/(#{["\\d", *str_to_digits.keys.map(&:reverse)].join("|")})/]
+      last = str_to_digits[last.reverse] unless last[/\d/]
+      [first, last].join.to_i
+    end
+  end
+end
---
+++ days/day02/day02.rb
@@ -0,0 +1,32 @@
+module Day02
+  def self.parse(input)
+    input.split("\n").map do |line|
+      _, data = line.split(":")
+      data.split(";").map do |set|
+        set.split(",").map {|c| n, color = c.split; [n.to_i, color]}
+      end
+    end
+  end
+
+  def self.part1(input)
+    to_match = {"red" => 12, "green" => 13, "blue" => 14}
+    parse(input).map.with_index(1) do |game, index|
+      game.all? do |sets|
+        sets.all? do |n, color|
+          n <= to_match[color]
+        end
+      end ? index : 0
+    end.sum
+  end
+
+  def self.part2(input)
+    parse(input).map do |game|
+      pairs = game.flatten.each_slice(2).to_a
+      best = {"red" => 0, "green" => 0, "blue" => 0}
+      pairs.each do |n, color|
+        best[color] = [best[color], n].max
+      end
+      best.values.reduce(:*)
+    end.sum
+  end
+end
