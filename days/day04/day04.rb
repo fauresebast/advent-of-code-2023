36ed208125b26bcfe65c10d5b1209e4f31b0d113
Sebastien Faure <sebastien@squadracer.com>
2023-12-04 17:40:44 +0100

    Add Day04

---
+++ days/day04/day04.rb
@@ -0,0 +1,34 @@
+module Day04
+  def self.parse(input)
+    cards = {}
+    input.each_line do |line|
+      card, series = line.split(": ")
+      winning, numbers = series.split(" | ").map{|s| s.split.map(&:to_i)}
+      cards[card[/\d+/].to_i] = [winning, numbers]
+    end
+    cards
+  end
+
+  def self.part1(input)
+    cards = parse(input)
+    cards.sum do |_, (winning, numbers)|
+      common = winning & numbers
+      common.size.zero? ? 0 : 2 ** (common.size - 1)
+    end
+  end
+
+  def self.part2(input)
+    cards = parse(input)
+    q = cards.to_a
+    sum = 0
+    while q.any?
+      id, (winning, numbers) = q.shift
+      sum += 1
+      common = winning & numbers
+      (id+1...id+1+common.size).each do |new_id|
+        q << [new_id, cards[new_id]]
+      end
+    end
+    sum
+  end
+end
---
+++ spec/day04_spec.rb
@@ -0,0 +1,22 @@
+require "./days/day04/day04"
+RSpec.describe Day04, "#solve" do
+  it "part 1 works with sample" do
+    str = File.read("./days/day04/sample")
+    expect(Day04.part1(str)).to eq 13
+  end
+
+  it "part 1 works with input" do
+    str = File.read("./days/day04/input")
+    expect(Day04.part1(str)).to eq 25010
+  end
+
+  it "part 2 works with sample" do
+    str = File.read("./days/day04/sample2")
+    expect(Day04.part2(str)).to eq 30
+  end
+
+  it "part 2 works with input" do
+    str = File.read("./days/day04/input")
+    expect(Day04.part2(str)).to eq 9924412
+  end
+end
