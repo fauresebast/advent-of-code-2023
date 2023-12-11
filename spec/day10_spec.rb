cb391d258051eccca2c2088e8307aa9e3fe3a4c3
Sebastien Faure <sebastien@squadracer.com>
2023-12-11 08:49:37 +0100

    Add Day10

---
+++ spec/day10_spec.rb
@@ -0,0 +1,32 @@
+require "./days/day10/day10"
+RSpec.describe Day10, "#solve" do
+  it "part 1 works with sample" do
+    str = File.read("./days/day10/sample")
+    expect(Day10.part1(str)).to eq 8
+  end
+
+  it "part 1 works with input" do
+    str = File.read("./days/day10/input")
+    expect(Day10.part1(str)).to eq 7063
+  end
+
+  it "part 2 works with sample2" do
+    str = File.read("./days/day10/sample2")
+    expect(Day10.part2(str)).to eq 4
+  end
+
+  it "part 2 works with sample3" do
+    str = File.read("./days/day10/sample3")
+    expect(Day10.part2(str)).to eq 8
+  end
+
+  it "part 2 works with sample4" do
+    str = File.read("./days/day10/sample4")
+    expect(Day10.part2(str)).to eq 10
+  end
+
+  it "part 2 works with input" do
+    str = File.read("./days/day10/input")
+    expect(Day10.part2(str)).to eq 589
+  end
+end
